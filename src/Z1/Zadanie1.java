86e567d4c8739d1fb3dce3b9002f59afbc5f116a
das1292 <jakub.klos.10@wp.pl>
2022-02-18 12:46:02 +0100

    Egzamin

--- src/Z1/Zadanie1.java
+++ src/Z1/Zadanie1.java
@@ -1,7 +1,28 @@
 package Z1;
 
+import java.time.LocalDate;
+import java.util.LinkedList;
+
 public class Zadanie1 {
     public static void main(String[] args){
-
+    LinkedList<Integer> int1=new LinkedList<>();
+    int1.add(1);
+    int1.add(2);
+    int1.add(3);
+    int1.add(4);
+    int1.add(5);
+    System.out.println(redukuj(int1,2));
+    LinkedList<LocalDate> LD1=new LinkedList<>();
+    LD1.add(LocalDate.of(2000,11,11));
+    LD1.add(LocalDate.of(2020,10,10));
+    LD1.add(LocalDate.of(2010,1,1));
+    LD1.add(LocalDate.of(2012,2,21));
+    System.out.println(redukuj(LD1,3));
+    }
+    public static <E extends LinkedList<?>>LinkedList redukuj(E list, int n){
+        for(int i=n-1;i<list.size();i=i+n-1){
+            list.remove(i);
+        }
+        return list;
     }
 }
--- src/Z3/Zadanie3.java
+++ src/Z3/Zadanie3.java
@@ -1,7 +1,14 @@
 package Z3;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Zadanie3 {
     public static void main(String[] args){
-
+        File file=new File("src/");
+        String[] str=file.list();
+        assert str!=null;
+        Arrays.stream(str).map(i->{if(!i.contains("."))return i;return null;}).filter(Objects::nonNull).forEach(System.out::println);
     }
 }
