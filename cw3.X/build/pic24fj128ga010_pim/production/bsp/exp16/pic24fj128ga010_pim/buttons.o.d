63152b5f9a41b9f948ebcfdd5f9d59bf49731306
das1292 <73549342+das1292@users.noreply.github.com>
2023-05-12 08:52:51 +0200

    Delete cw3.X directory

--- cw3.X/build/pic24fj128ga010_pim/production/bsp/exp16/pic24fj128ga010_pim/buttons.o.d
+++
@@ -1,5 +0,0 @@
-build/pic24fj128ga010_pim/production/bsp/exp16/pic24fj128ga010_pim/buttons.o: \
- bsp/exp16/pic24fj128ga010_pim/buttons.c \
- bsp/exp16/pic24fj128ga010_pim/buttons.h
-
-bsp/exp16/pic24fj128ga010_pim/buttons.h:
--- cw3.X/build/pic24fj128ga010_pim/production/bsp/exp16/pic24fj128ga010_pim/leds.o.d
+++
@@ -1,5 +0,0 @@
-build/pic24fj128ga010_pim/production/bsp/exp16/pic24fj128ga010_pim/leds.o: \
- bsp/exp16/pic24fj128ga010_pim/leds.c \
- bsp/exp16/pic24fj128ga010_pim/leds.h
-
-bsp/exp16/pic24fj128ga010_pim/leds.h:
